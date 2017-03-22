6d48db093cb7c71f8b849e68e57e38816e7e1b41
chrisbirie <chris.birie@gmail.com>
2017-03-21 23:48:40 -0400

    added regions to pokemon and added pokemon items and types

--- lib/faker/pokemon.rb
+++ lib/faker/pokemon.rb
@@ -1,12 +1,50 @@
 module Faker
   class Pokemon < Base
     class << self
-      def name
-        fetch('pokemon.names')
+      def name(region = '')
+        case region
+          when 'Kanto'
+            fetch('pokemon.kanto_names')
+          when 'Johto'
+            fetch('pokemon.johto_names')
+          when 'Hoenn'
+            fetch('pokemon.hoenn_names')
+          when 'Sinnoh'
+            fetch('pokemon.sinnoh_names')
+          when 'Unova'
+            fetch('pokemon.unova_names')
+          when 'Kalos'
+            fetch('pokemon.kalos_names')
+          when 'Alola'
+            fetch('pokemon.alola_names')
+          else
+            array_of_names = []
+            array_of_names << fetch('pokemon.kanto_names')
+            array_of_names << fetch('pokemon.johto_names')
+            array_of_names << fetch('pokemon.hoenn_names')
+            array_of_names << fetch('pokemon.sinnoh_names')
+            array_of_names << fetch('pokemon.unova_names')
+            array_of_names << fetch('pokemon.kalos_names')
+            array_of_names << fetch('pokemon.alola_names')
+            array_of_names.sample
+        end
+
       end
 
       def location
         fetch('pokemon.locations')
+      end
+
+      def item
+        fetch('pokemon.items')
+      end
+
+      def move
+        fetch('pokemon.moves')
+      end
+
+      def type
+        fetch('pokemon.types')
       end
     end
   end
--- test/test_faker_pokemon.rb
+++ test/test_faker_pokemon.rb
@@ -9,8 +9,48 @@ class TestFakerPokemon < Test::Unit::TestCase
     assert @tester.name.match(/\w+/)
   end
 
+  def test_name_kanto
+    assert @tester.name('Kanto').match(/\w+/)
+  end
+
+  def test_name_johto
+    assert @tester.name('Johto').match(/\w+/)
+  end
+
+  def test_name_hoenn
+    assert @tester.name('Hoenn').match(/\w+/)
+  end
+
+  def test_name_sinnoh
+    assert @tester.name('Sinnoh').match(/\w+/)
+  end
+
+  def test_name_unova
+    assert @tester.name('Unova').match(/\w+/)
+  end
+
+  def test_name_kalos
+    assert @tester.name('Kalos').match(/\w+/)
+  end
+
+  def test_name_alola
+    assert @tester.name('Alola').match(/\w+/)
+  end
+
   def test_location
     assert @tester.location.match(/\w+/)
+  end
+
+  def test_item
+    assert @tester.item.match(/\w+/)
+  end
+
+  def test_move
+    assert @tester.move.match(/\w+/)
+  end
+
+  def test_type
+    assert @tester.type.match(/\w+/)
   end
 
 end
